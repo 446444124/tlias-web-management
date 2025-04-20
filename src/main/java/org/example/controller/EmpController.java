27f67043940fe6076d14be4bf0f27074128ecffc
lunxi <446444124@qq.com>
2025-04-20 17:02:40 +0800

    feat(tlias-web-management): 添加员工相关实体类和接口
    - 新增 Emp、EmpExpr 类以及相关 Mapper、Service 和 Controller
    -为员工管理和工作经历提供数据模型和基本的接口定义

---
+++ src/main/java/org/example/controller/EmpController.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmpController {
+    private static final Logger log = LoggerFactory.getLogger(DeptController.class);
+
+}
